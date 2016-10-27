4a5a7fa01ea5ae020a5a91dd3826ed5816e38f3e
Anirudh <ramanathana@google.com>
2016-10-26 20:48:18 -0700

    Fixing e2e tests.

--- test/e2e/petset.go
+++ test/e2e/petset.go
@@ -118,7 +118,7 @@ var _ = framework.KubeDescribe("PetSet [Slow] [Feature:PetSet]", func() {
 			pst := petSetTester{c: c}
 
 			By("Saturating pet set " + ps.Name)
-			pst.saturate(ps)
+			pst.waitForRunning(ps.Spec.Replicas, ps)
 
 			By("Verifying petset mounted data directory is usable")
 			ExpectNoError(pst.checkMount(ps, "/data"))
@@ -129,7 +129,7 @@ var _ = framework.KubeDescribe("PetSet [Slow] [Feature:PetSet]", func() {
 
 			By("Restarting pet set " + ps.Name)
 			pst.restart(ps)
-			pst.saturate(ps)
+			pst.waitForRunning(ps.Spec.Replicas, ps)
 
 			By("Verifying petset mounted data directory is usable")
 			ExpectNoError(pst.checkMount(ps, "/data"))
@@ -144,7 +144,7 @@ var _ = framework.KubeDescribe("PetSet [Slow] [Feature:PetSet]", func() {
 
 			petMounts := []api.VolumeMount{{Name: "datadir", MountPath: "/data/"}}
 			podMounts := []api.VolumeMount{{Name: "home", MountPath: "/home"}}
-			ps := newPetSet(psName, ns, headlessSvcName, 2, petMounts, podMounts, labels)
+			ps := newPetSet(psName, ns, headlessSvcName, 1, petMounts, podMounts, labels)
 			_, err := c.Apps().PetSets(ns).Create(ps)
 			Expect(err).NotTo(HaveOccurred())
 
@@ -152,8 +152,8 @@ var _ = framework.KubeDescribe("PetSet [Slow] [Feature:PetSet]", func() {
 
 			pst.waitForRunning(1, ps)
 
-			By("Marking pet at index 0 as healthy.")
-			pst.setHealthy(ps)
+			By("Scaling petset to 2 replicas.")
+			pst.setReplicas(2, ps)
 
 			By("Waiting for pet at index 1 to enter running.")
 			pst.waitForRunning(2, ps)
@@ -172,7 +172,7 @@ var _ = framework.KubeDescribe("PetSet [Slow] [Feature:PetSet]", func() {
 			pst.deletePetAtIndex(1, ps)
 
 			By("Confirming all pets in petset are created.")
-			pst.saturate(ps)
+			pst.waitForRunning(ps.Spec.Replicas, ps)
 		})
 	})
 
@@ -570,17 +570,6 @@ func (p *petSetTester) execInPets(ps *apps.PetSet, cmd string) error {
 	return nil
 }
 
-func (p *petSetTester) saturate(ps *apps.PetSet) {
-	// TODO: Watch events and check that creation timestamps don't overlap
-	var i int32
-	for i = 0; i < ps.Spec.Replicas; i++ {
-		framework.Logf("Waiting for pet at index " + fmt.Sprintf("%v", i+1) + " to enter Running")
-		p.waitForRunning(i+1, ps)
-		framework.Logf("Marking pet at index " + fmt.Sprintf("%v", i) + " healthy")
-		p.setHealthy(ps)
-	}
-}
-
 func (p *petSetTester) deletePetAtIndex(index int, ps *apps.PetSet) {
 	// TODO: we won't use "-index" as the name strategy forever,
 	// pull the name out from an identity mapper.
@@ -686,24 +675,14 @@ func (p *petSetTester) waitForRunning(numPets int32, ps *apps.PetSet) {
 	if pollErr != nil {
 		framework.Failf("Failed waiting for pods to enter running: %v", pollErr)
 	}
-
 	p.waitForStatus(ps, numPets)
 }
 
-func (p *petSetTester) setHealthy(ps *apps.PetSet) {
-	podList := p.getPodList(ps)
-	markedHealthyPod := ""
-	for _, pod := range podList.Items {
-		if pod.Status.Phase != api.PodRunning {
-			framework.Failf("Found pod in %v cannot set health", pod.Status.Phase)
-		}
-		if isInitialized(pod) {
-			continue
-		}
-		if markedHealthyPod != "" {
-			framework.Failf("Found multiple non-healthy pets: %v and %v", pod.Name, markedHealthyPod)
-		}
-		markedHealthyPod = pod.Name
+func (p *petSetTester) setReplicas(replicas int, ps *apps.PetSet) {
+	ps.Spec.Replicas = replicas
+	_, err := p.c.Apps().PetSets(ps.Namespace).Update(ps)
+	if err != nil {
+		framework.Failf("Failed to update scale of PetSet to %d: %v", n, err)
 	}
 }
 
